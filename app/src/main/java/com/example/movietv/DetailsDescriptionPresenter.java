5eaeb64e484a5a20cf9bd7b41b9867c895ce0b2b
Equipo 8 <arielsb.ti19@utsjr.edu.mx>
2022-07-30 19:17:23 -0500

    Envio V.1

---
+++ app/src/main/java/com/example/movietv/DetailsDescriptionPresenter.java
@@ -0,0 +1,17 @@
+package com.example.movietv;
+
+import androidx.leanback.widget.AbstractDetailsDescriptionPresenter;
+
+public class DetailsDescriptionPresenter extends AbstractDetailsDescriptionPresenter {
+
+    @Override
+    protected void onBindDescription(ViewHolder viewHolder, Object item) {
+        com.example.movietv.Movie movie = (com.example.movietv.Movie) item;
+
+        if (movie != null) {
+            viewHolder.getTitle().setText(movie.getTitle());
+            viewHolder.getSubtitle().setText(movie.getStudio());
+            viewHolder.getBody().setText(movie.getDescription());
+        }
+    }
+}
